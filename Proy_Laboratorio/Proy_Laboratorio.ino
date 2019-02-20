5a045dacbae69c6d098a96032358d71338ad1bcf
Juanesgallego <juanegallegob@gmail.com>
2019-02-20 14:48:28 -0500

    Incorporacion del Bluetooth

--- Proy_Laboratorio/Proy_Laboratorio.ino
+++ Proy_Laboratorio/Proy_Laboratorio.ino
@@ -1,24 +1,34 @@
 #include <Servo.h> 
 #define PINSERVODERECHO 5 //pin del servo derecho como 5
 #define PINSERVOIZQUIERDO 7 //pin del servo derecho como 7
+#define PinTX_BT 10
+#define PinRX_BT 9
 Servo ServoDerecho; 
 Servo ServoIzquierdo;
+#include <SoftwareSerial.h>
+
 
 char tecla;
-
+SoftwareSerial Bt(PinRX_BT, PinTX_BT);
 void setup()
 {
 
  ServoDerecho.attach(PINSERVODERECHO);
  ServoIzquierdo.attach(PINSERVOIZQUIERDO);
  
-  Serial.begin(9600);   // comunicacion de monitor serial a 9600 bps
+ Serial.begin(9600);   // comunicacion de monitor serial a 9600 bps
+ Bt.begin(9600);
 }
 
 void loop(){
 if (Serial.available()>0)
 {
   tecla = Serial.read();
+}
+if(Bt.available()>0)
+{
+tecla=Bt.read();
+}
   switch(tecla)
   {
     case 'w':
@@ -41,6 +51,5 @@ if (Serial.available()>0)
     ServoDerecho.write(90);
     break;
   }
-}
 
 }
