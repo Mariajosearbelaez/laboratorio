a5b1a6d69bf26ef25c8a04dc75c8e3e26b2dd47a
Juanesgallego <juanegallegob@gmail.com>
2019-02-21 02:47:14 -0500

    preparación comentarios

--- Proy_Laboratorio/Proy_Laboratorio.ino
+++ Proy_Laboratorio/Proy_Laboratorio.ino
@@ -1,67 +1,86 @@
-#include <Servo.h> 
+/*
+Laboratorio #2 de programación.
+Hecho por Maria José Arbelaez y Juan Esteban Gallego
+Robot inalámbrico denominado Poppet
+21/02/2019
+*/
+#include <Servo.h> //Librería del Servo
+#include <SoftwareSerial.h>//Librería Bluetooth
+
 #define PINSERVODERECHO 5 //pin del servo derecho como 5
 #define PINSERVOIZQUIERDO 7 //pin del servo derecho como 7
-#define PinTX_BT 10
-#define PinRX_BT 9
-Servo ServoDerecho; 
+#define PinTX_BT 10 //Pin del Tx del Bluetooth
+#define PinRX_BT 9 //Pin del Tx del Bluetooth
+#define Pin_Echo 6  //Pin Echo del Ultrasonido
+#define Pin_Trigger 8 // Pin Trigger del Ultrasonido
+
+
+long duracion, dist; // Variables tipo float para el Ultrasonido
+char tecla; // Variable que almacena las órdenes del teclado
+
+
+SoftwareSerial Bt(PinRX_BT, PinTX_BT); //Conexión del Bluetooth al Arduino
+Servo ServoDerecho; // Declaración de los Servos
 Servo ServoIzquierdo;
-#include <SoftwareSerial.h>
-#define Pin_Echo 6
-#define Pin_Trigger 8
-long duracion, dist;
-char tecla;
-SoftwareSerial Bt(PinRX_BT, PinTX_BT);
+
+
 void setup()
 {
 
- ServoDerecho.attach(PINSERVODERECHO);
+ ServoDerecho.attach(PINSERVODERECHO); // Conexión de los Servos al Arduino
  ServoIzquierdo.attach(PINSERVOIZQUIERDO);
  
  Serial.begin(9600);   // comunicacion de monitor serial a 9600 bps
- Bt.begin(9600);
+ Bt.begin(9600);       // Comunicación del Bluetooth a 9600 bps
 }
 
 void loop(){
-if (Serial.available()>0)
+if (Serial.available()>0) // Lectura del valor del Teclado
 {
   tecla = Serial.read();
 }
-if(Bt.available()>0)
+if(Bt.available()>0) // Lectura del valor puesto desde la aplicación
 {
 tecla=Bt.read();
 }
-  switch(tecla)
+  switch(tecla) // Movimiento según cada caso
   {
-    case 'w':
+    case 'w': // Avanzar
     ServoIzquierdo.write(0);
     ServoDerecho.write(180);
     break;
-     case 's':
+     case 's': // Retroceder
     ServoIzquierdo.write(180);
     ServoDerecho.write(0);
     break; 
-    case 'a':
+    case 'a': // Girar a la izquierda
     ServoIzquierdo.write(90);
     ServoDerecho.write(0);
-    break; case 'd':
+    break;
+    case 'd': // Girar a la Derecha
     ServoIzquierdo.write(180);
     ServoDerecho.write(90);
     break;
-    case 'q':
+    case 'q': // Detenerse
     ServoIzquierdo.write(90);
     ServoDerecho.write(90);
     break;
   }
-digitalWrite(Pin_Trigger, LOW);
+digitalWrite(Pin_Trigger, LOW); // Pulso emitido por el Trigger
   delayMicroseconds(2);
   digitalWrite(Pin_Trigger, HIGH); 
   delayMicroseconds(10);
   digitalWrite(Pin_Trigger, LOW);
-  duracion = pulseIn(Pin_Echo, HIGH);
-  dist = (duracion/2) / 29;
-  if (dist <= 5 && dist >= 1)
+
+  
+  duracion = pulseIn(Pin_Echo, HIGH);  // Tiempo en el que el Echo percibió el pulso
+  
+  dist = (duracion/2) / 29; // Cálculo de la distancia en cm
+  
+  if (dist <= 5 && dist >= 1) // Orden de detenerse si percibe algo a menos de 5 cm
   {
     ServoIzquierdo.write(90);
     ServoDerecho.write(90);
   }
+  delay(400); 
 }
